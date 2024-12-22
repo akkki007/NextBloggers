6c5829590f6f7dd7342fc6dbee94b5d28f60dfa5
Akshay Nazare <akshaynazare3@gmail.com>
2024-12-22 14:32:17 +0530

    commit

--- app/api/lib/db.js
+++ app/api/lib/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       return;
     }
 
-    await mongoose.connect("mongodb://localhost:27017/blogging");
+    await mongoose.connect("mongodb://localhost:27017/blogging", { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
