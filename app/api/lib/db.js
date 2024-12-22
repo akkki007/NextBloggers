41bd3314ca3545d8e959d48798c18195cac8685b
Akshay Nazare <akshaynazare3@gmail.com>
2024-12-22 23:26:53 +0530

    m

--- app/api/lib/db.js
+++ app/api/lib/db.js
@@ -1,20 +1,13 @@
 // app/api/lib/db.js
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
 const connectDB = async () => {
   try {
     if (mongoose.connections[0].readyState) {
       return;
     }
-    const MONGODB_URI = process.env.MONGODB_URI;
     await mongoose.connect(
-      "mongodb+srv://akki3:akshay1234@blogging-website.q0rsg.mongodb.net/?retryWrites=true&w=majority&appName=Blogging-Website",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://akki3:akshay1234@blogging-website.q0rsg.mongodb.net/?retryWrites=true&w=majority&appName=Blogging-Website"
     );
     console.log("Connected to MongoDB");
   } catch (error) {
